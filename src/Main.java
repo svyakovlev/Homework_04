22590706cb5b9b5c7f1685bc35ed43e66f6ec863
Сергей <s.v.yakovlev@yandex.ru>
2022-06-13 20:34:56 +0400

    Готово задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 //Задание №1
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (i = 10; i >= 1; i = i - 1) {
+            System.out.print(i + " ");
+        }
     }
 }
